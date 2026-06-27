2e3b160d5a2cf3ae863aab1fd80a71fdccf0738f
Benedikt Weyer <bw.development@pm.me>
2026-06-27 13:43:24 +0200

    feat: Add flake configuration for development shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development shell for uv-quick-tool-creator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python314
+            uv
+          ];
+
+          UV_PYTHON_DOWNLOADS = "never";
+        };
+      });
+    };
+}
